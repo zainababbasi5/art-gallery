e2bc180a7a6e252a69346c31bad5d09db794aa66
zainababbasi5 <zainababbasi605@gmail.com>
2024-09-01 21:12:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+let themeToggler = document.querySelector(".theme-toggler");
+let toggleBtn = document.querySelector(".toggle-btn");
+
+toggleBtn.onclick = () => {
+  themeToggler.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  themeToggler.classList.remove("active");
+};
+
+document.querySelectorAll(".theme-toggler .theme-btn").forEach((btn) => {
+  btn.onclick = () => {
+    let color = btn.style.background;
+    document.querySelector(":root").style.setProperty("--theme-color", color);
+  };
+});
+
+
+document.getElementById('add-to-cart-btn').addEventListener('click', function() {
+    // Code to handle the Add to Cart action
+    alert('Product added to cart!');
+    // You can also update your cart or call a function here
+});
